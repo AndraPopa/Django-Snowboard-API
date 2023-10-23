7d2e23e8f2426aa89b1c0955d0bd49dfad13a4b9
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2023-10-23 21:21:10 +0300

    get the values from the snow form

--- snowboardsapi/snowboards/views.py
+++ snowboardsapi/snowboards/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Snowboard
 from .serializers import SnowboardSerializer
@@ -12,7 +13,17 @@ class SnowboardView(viewsets.ModelViewSet):
 class ChooseSnowboardView(TemplateView):
     template_name = 'snow_form.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['name'] = 'Rider!'
-        return context
+    def get(self, request, *arg, **kwargs):
+        return render(request, self.template_name, {'name': 'Rider!'})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == "POST":
+            boy = request.POST.get('boy', False)
+            girl = request.POST.get('girl', False)
+            freestyle = request.POST.get('freestyle', False)
+            freeride = request.POST.get('freeride', False)
+            both = request.POST.get('both', False)
+            height = request.POST['height']
+            skills = request.POST['skills']
+
+        return render(request, self.template_name, {'name':  girl})
