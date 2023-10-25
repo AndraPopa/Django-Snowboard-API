db9c440de08e95cc0fb575dd53a0d99da7417fac
AndraPopa <93611644+AndraPopa@users.noreply.github.com>
2023-10-26 00:03:00 +0300

    added logic for sizing boards based on heights

--- snowboardsapi/snowboards/views.py
+++ snowboardsapi/snowboards/views.py
@@ -44,16 +44,47 @@ class YourNextSnowboardView(ListView):
         skills = request.GET.get('skills')
         style = request.GET.get('style')
         height = request.GET.get('height')
-        print(height)
         rider_name = request.GET.get('rider_name').split("'")[1].split("'")[0]
         filter = process_queryset(gender, skills, style)
+        size_range = None
+        if height:
+            size_range = process_size_range(int(height), style)
         queryset = Snowboard.objects.filter(
             gender__in=[filter['gender'], 'Unisex'],
             style=filter['style'],
             level=filter['level'],
         )
-        return render(request, self.template_name,
-                      {'gender': gender, 'snowboards': queryset, 'rider_name': rider_name})
+        return render(request,
+                      self.template_name,
+                      {'gender': gender, 'snowboards': queryset, 'rider_name': rider_name, 'size_range': size_range})
+
+
+freeride_size_chart = {
+    'lt_160': '145-151',
+    'gt_160_lt_170': '151-155',
+    'gt_170_lt_180': '155-158',
+    'gt_180': '158-160'
+}
+
+park_size_chart = {
+    'lt_160': '140-145',
+    'gt_160_lt_170': '145-149',
+    'gt_170_lt_180': '149-153',
+    'gt_180': '153-156'
+}
+
+
+def process_size_range(height, style):
+    size_range = None
+    if height < 160:
+        size_range = park_size_chart['lt_160'] if style == 'park' else freeride_size_chart['lt_160']
+    elif 160 <= height < 170:
+        size_range = park_size_chart['gt_160_lt_170'] if style == 'park' else freeride_size_chart['gt_160_lt_170']
+    elif 170 <= height < 180:
+        size_range = park_size_chart['gt_170_lt_180'] if style == 'park' else freeride_size_chart['gt_170_lt_180']
+    elif height > 180:
+        size_range = park_size_chart['gt_180'] if style == 'park' else freeride_size_chart['gt_180']
+    return size_range
 
 
 def process_queryset(gender, skills, style):
